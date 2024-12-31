566947a0c1b5fd5489a3915bc61c1e3e9942d459
bahakarakaya <bhkrky@gmail.com>
2024-12-31 15:05:35 +0300

    added collision properties

--- my_robot_description/build/my_robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/babuntu/ros2_ws/src/my_robot_description
-# Build directory: /home/babuntu/ros2_ws/src/my_robot_description/build/my_robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
